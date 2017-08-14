23c3c1dfb2a3435ce92ea6b839483896a6a3786e
Dennis Collaris <d.collaris@me.com>
2017-08-14 14:09:20 +0200

    Prune rule parts based on local increment score

--- inTrees/R/pruneRule.R
+++ inTrees/R/pruneRule.R
@@ -1,8 +1,26 @@
-pruneRule <-
-function(rules,X,target, maxDecay = 0.05, typeDecay = 2){
-  newRuleMetric <- NULL
-  for(i in 1:nrow(rules)){
-    newRuleMetric <- rbind(newRuleMetric, pruneSingleRule(rules[i,],X,target, maxDecay, typeDecay))
-  }
+# pruneRule <-
+# function(rules,X,target, maxDecay = 0.05, typeDecay = 2){
+#   newRuleMetric <- NULL
+#   for(i in 1:nrow(rules)){
+#     newRuleMetric <- rbind(newRuleMetric, pruneSingleRule(rules[i,],X,target, maxDecay, typeDecay))
+#   }
+#   return(newRuleMetric)
+# }
+
+pruneRule <- function(rules, maxDecay = 0.05){
+  # newRuleMetric <- NULL
+  # for(i in 1:nrow(rules)){
+  # 	print(i)
+  #   newRuleMetric <- rbind(newRuleMetric, pruneSingleRule(rules[i,],maxDecay))
+  # }
+  newRuleMetric <- apply(rules, 1, function(rule){
+  	return(pruneSingleRule(rule, maxDecay))
+  })
+
+  newRuleMetric = data.frame(do.call(rbind, newRuleMetric))
+  newRuleMetric = newRuleMetric[newRuleMetric$len != 0,]
+  
+  cat(paste(NROW(rules$ruleSet) - NROW(newRuleMetric)," paths are ignored.",sep=""))
   return(newRuleMetric)
 }
+
--- inTrees/R/pruneSingleRule.R
+++ inTrees/R/pruneSingleRule.R
@@ -1,33 +1,43 @@
-pruneSingleRule <-
-function(rule, X, target, maxDecay, typeDecay){
-# typeDecay = 1: relative error increase; otherwise: absolute error increase
+# pruneSingleRule <-
+# function(rule, X, target, maxDecay, typeDecay){
+# # typeDecay = 1: relative error increase; otherwise: absolute error increase
 
-  #A <- gregexpr("X\\[,[0-9]+\\]", s)
-  newRuleMetric <- measureRule(data.frame(t(rule[c("condition", "pred")])),X,target)
-  errOrig <- as.numeric(newRuleMetric["err"])  
-  ruleV <- unlist(strsplit(rule["condition"],split= " & "))
-  pred <- rule["pred"]
-  # newRule <- NULL
-  if(length(ruleV)==1) return(newRuleMetric)
-  for(i in length(ruleV):1){
-    restRule <- ruleV[-i]
-    restRule <- paste(restRule,collapse= " & ")
-    metricTmp <- measureRule(restRule,X,target,pred)
-    errNew <- as.numeric(metricTmp["err"]) 
-    if(typeDecay == 1){
-      decay <- (errNew-errOrig)/max(errOrig,0.000001)
-    }else{
-      decay <- (errNew-errOrig)
-    }      
-    if( decay <= maxDecay){
-    #if( errNew-errOrig <= maxDecay){
-      ruleV <- ruleV[-i] 
-      # newRule saves the last changed rule and metrics
-      newRuleMetric <- metricTmp
-      if(length(ruleV)<=1)break
-    }
-  }
-  return(newRuleMetric)
-  #rule["condition"] <- paste(ruleV,collapse= " & ")
-  #return(rule)
+#   #A <- gregexpr("X\\[,[0-9]+\\]", s)
+#   newRuleMetric <- measureRule(data.frame(t(rule[c("condition", "pred", "li")])),X,target)
+#   errOrig <- as.numeric(newRuleMetric["err"])  
+
+#   ruleV <- unlist(strsplit(rule["condition"],split= " & "))
+#   pred <- rule["pred"]
+#   # newRule <- NULL
+#   if(length(ruleV)==1) return(newRuleMetric)
+#   for(i in length(ruleV):1){
+#     restRule <- ruleV[-i]
+#     restRule <- paste(restRule,collapse= " & ")
+#     metricTmp <- measureRule(cbind.data.frame(ruleExec=restRule, pred=rule["pred"], li=rule["li"]),X,target,pred)
+#     errNew <- as.numeric(metricTmp["err"]) 
+#     if(typeDecay == 1){
+#       decay <- (errNew-errOrig)/max(errOrig,0.000001)
+#     }else{
+#       decay <- (errNew-errOrig)
+#     }      
+#     if( decay <= maxDecay){
+#     #if( errNew-errOrig <= maxDecay){
+#       ruleV <- ruleV[-i] 
+#       # newRule saves the last changed rule and metrics
+#       newRuleMetric <- metricTmp
+#       if(length(ruleV)<=1)break
+#     }
+#   }
+#   return(newRuleMetric)
+#   #rule["condition"] <- paste(ruleV,collapse= " & ")
+#   #return(rule)
+# }
+
+
+pruneSingleRule <-
+function(rule, maxDecay){
+  rule$ruleSet = rule$ruleSet[abs(unlist(rule$liSet))>maxDecay]
+  rule$len = NROW(rule$ruleSet)
+  rule$liSet = rule$liSet[abs(unlist(rule$liSet))>maxDecay]
+  return(rule)
 }
