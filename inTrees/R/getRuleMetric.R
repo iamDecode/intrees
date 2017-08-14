81bac193b7d9841d9c02e260bd2b0d0d6279da3a
Dennis Collaris <d.collaris@me.com>
2017-08-14 12:46:17 +0200

    Rewrite getRuleMetric to operate on ruleList instead of Exec format, and leave ruleSet/liSet columns in tact

--- inTrees/R/getRuleMetric.R
+++ inTrees/R/getRuleMetric.R
@@ -1,18 +1,19 @@
 getRuleMetric <-
-function(ruleExec, X, target){
+function(rulesList, X, target){
   #typeX = getTypeX(X)
   #ruleExec <- unique(t(sapply(allRulesList,RuleList2Exec,typeX=typeX)))
   #colnames(ruleExec) <- c("len","condition")
 
-  ruleMetric <- t(apply(ruleExec[,c('condition', 'pred')],1,measureRule,X,target))
-  rownames(ruleMetric) = NULL; 
+  ruleMetric <- t(apply(rulesList,1,measureRule,X,target))
+
+  # rownames(ruleMetric) = NULL; 
   # ruleMetric <- cbind( ruleExec[,1] ,  ruleMetric )
-  colnames(ruleMetric) <- c("len","freq","err","condition","pred")
-  dIx <- which(ruleMetric[,"len"]=="-1") 
-  if(length(dIx)>0){
-   ruleMetric <- ruleMetric[-dIx,]
-   print(paste( length(dIx)," paths are ignored.",sep=""))
-  }
+  # 
+  ruleMetric = data.frame(do.call(rbind, ruleMetric[!sapply(ruleMetric, is.null)]))
+  colnames(ruleMetric) <- c("len","freq","err","ruleSet","prediction","liSet")
+
+  cat(paste(NROW(rulesList$ruleSet) - NROW(ruleMetric)," paths are ignored.",sep=""))
+
   return(ruleMetric)
   #qIx = order((1- as.numeric(ruleMetric[,"err"])),
   #            as.numeric(ruleMetric[,"freq"]),
